3609cbd7b63ac8e0196618819474702ffbf4ec6c
Rajamohan4487 <rajamohan4487@gmail.com>
2026-03-07 19:04:51 +0530

    VPC Module

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "roboshop" {
+resource "aws_vpc" "var.project" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
   enable_dns_support = true
@@ -8,7 +8,7 @@ resource "aws_vpc" "roboshop" {
   }
 }
 
-resource "aws_internet_gateway" "main" {
+resource "aws_internet_gateway" "var.project" {
   vpc_id = aws_vpc.roboshop.id #vpc association
 
   tags = {
@@ -18,7 +18,7 @@ resource "aws_internet_gateway" "main" {
 
 resource "aws_subnet" "public" { 
   count = length(var.public_sub_cidr)
-  vpc_id     = aws_vpc.roboshop.id
+  vpc_id     = aws_vpc.var.project.id
   cidr_block = var.public_sub_cidr[count.index]
   availability_zone = data.aws_availability_zones.available_zones.names[count.index]
   map_public_ip_on_launch = true
@@ -30,7 +30,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private" {             
   count = length(var.private_sub_cidr)
-  vpc_id     = aws_vpc.roboshop.id
+  vpc_id     = aws_vpc.var.project.id
   cidr_block = var.private_sub_cidr[count.index]
   availability_zone = data.aws_availability_zones.available_zones.names[count.index]
 
@@ -41,7 +41,7 @@ resource "aws_subnet" "private" {
 
 resource "aws_subnet" "database" {
   count = length(var.database_sub_cidr)
-  vpc_id     = aws_vpc.roboshop.id
+  vpc_id     = aws_vpc.var.project.id
   cidr_block = var.database_sub_cidr[count.index]
   availability_zone = data.aws_availability_zones.available_zones.names[count.index]
 
